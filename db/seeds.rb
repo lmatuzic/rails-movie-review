55f20ed3e1dcaa8e36e2b374ddf65d01055942bd
Luka <luka@pop-os.localdomain>
2020-12-13 19:53:32 +0100

    added seed data, added show page for individual movie

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,14 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+   
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+movies = Movie.create(
+  [
+    { title: 'Star Wars', rating: '4 stars', total_gross: 70000000000, released_on: '1983-11-23', description: "Star Wars is an American epic space opera media franchise created by George Lucas, which began with the eponymous 1977 film and quickly became a worldwide pop-culture phenomenon. The franchise has been expanded into various films and other media, including television series, video games, novels, comic books, theme park attractions, and themed areas, comprising an all-encompassing fictional universe." },
+    { title: 'Lord Of The Rings', rating: '5 stars', total_gross: 34000000000, released_on: '2001-01-12', description: "The Lord of the Rings is an epic high fantasy novel by the English author and scholar J. R. R. Tolkien. Set in Middle-earth, the world at some distant time in the past, the story began as a sequel to Tolkien's 1937 children's book The Hobbit, but eventually developed into a much larger work. Written in stages between 1937 and 1949, The Lord of the Rings is one of the best-selling books ever written, with over 150 million copies sold." },
+    { title: 'Harry Potter', rating: '5 stars', total_gross: 48000000000, released_on: '2003-07-11', description: "Harry Potter is a series of seven fantasy novels written by British author J. K. Rowling. The novels chronicle the lives of a young wizard, Harry Potter, and his friends Hermione Granger and Ron Weasley, all of whom are students at Hogwarts School of Witchcraft and Wizardry. The main story arc concerns Harry's struggle against Lord Voldemort, a dark wizard who intends to become immortal, overthrow the wizard governing body known as the Ministry of Magic and subjugate all wizards and Muggles (non-magical people)." },
+  ]
+)
