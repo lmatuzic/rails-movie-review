78b2cdd63d5820a5d1c75a5c0e1b52a067827a82
Luka <luka@pop-os.localdomain>
2020-12-15 18:14:15 +0100

    added edit movie routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "movies#index"
   get "movies", to: "movies#index"
-  get "movies/:id", to: "movies#show"
+  get "movies/:id", to: "movies#show", as: "movie"
+  get "movies/:id/edit", to: "movies#edit", as: "edit_movie"
 end
