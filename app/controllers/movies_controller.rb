524c8d38c434009e68aa6d2c4c7c3b1113c65c27
Luka <luka@pop-os.localdomain>
2020-12-15 21:46:42 +0100

    added new and create movie functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,15 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
 
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.create(movie_params)
+    render :show
+  end
+
   def show
     @movie = Movie.find(params[:id])
   end
