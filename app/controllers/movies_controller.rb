d64f26c04babb50c9da61a6bf9002019bfe13561
Luka <luka@pop-os.localdomain>
2020-12-15 21:28:23 +0100

    added update movie functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,4 +10,15 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find(params[:id])
   end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    render :show
+  end
+
+  private 
+    def movie_params
+      params.require(:movie).permit(:title, :rating, :total_gross, :description, :released_on)
+    end
 end
